e8fd01308f570c4edb93f38bedf0d7f088775e3d
Valeria <lerabicher@gmail.com>
2026-01-28 18:09:05 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-# write your code here
+from __future__ import annotations
+from typing import List
+
+
+class Animal:
+    alive: List["Animal"] = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name: str = name
+        self.health: int = health
+        self.hidden: bool = False
+        Animal.alive.append(self)
+
+    def die(self) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, target: Animal) -> None:
+        if not isinstance(target, Herbivore):
+            return
+
+        if target.hidden:
+            return
+
+        target.health -= 50
+
+        if target.health <= 0:
+            target.health = 0
+            target.die()
