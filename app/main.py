36b1fa30fe69325642fb7e331279572a794cc7a9
Valeria <lerabicher@gmail.com>
2026-01-28 18:20:29 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,43 +1,49 @@
-from __future__ import annotations
-from typing import List
+from typing import List, ClassVar
 
 
 class Animal:
-    alive: List["Animal"] = []
+    alive: ClassVar[List["Animal"]] = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False,
+    ) -> None:
         self.name: str = name
         self.health: int = health
-        self.hidden: bool = False
+        self.hidden: bool = hidden
         Animal.alive.append(self)
 
-    def die(self) -> None:
-        if self in Animal.alive:
-            Animal.alive.remove(self)
-
-    def __repr__(self) -> str:
+    def __repr__(
+        self,
+    ) -> str:
         return (
             f"{{Name: {self.name}, "
             f"Health: {self.health}, "
             f"Hidden: {self.hidden}}}"
         )
 
+    def die(
+        self,
+    ) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
 
 class Herbivore(Animal):
-    def hide(self) -> None:
+    def hide(
+        self,
+    ) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(self, target: Animal) -> None:
-        if not isinstance(target, Herbivore):
-            return
-
-        if target.hidden:
-            return
-
-        target.health -= 50
-
-        if target.health <= 0:
-            target.health = 0
-            target.die()
+    def bite(
+        self,
+        animal: Animal,
+    ) -> None:
+        if isinstance(animal, Herbivore) and not animal.hidden:
+            animal.health -= 50
+            if animal.health <= 0:
+                animal.die()
